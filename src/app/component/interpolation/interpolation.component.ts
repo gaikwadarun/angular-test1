f62b98c7559ea179fa6625fc30b5e8cd3f6e167c
gaikwadarun <118682233+gaikwadarun@users.noreply.github.com>
2022-12-30 00:32:58 +0530

    bootstrap changes

--- src/app/component/interpolation/interpolation.component.ts
+++ src/app/component/interpolation/interpolation.component.ts
@@ -2,19 +2,10 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-interpolation',
-  template:`
-  <h1> Hello Good morning {{name}}<h1>
-  <h2> Hii {{name}}. </h2>
-  <button (click)="onClick()">Submit </button>{{greeting}}
-  `
-  
+  templateUrl: './interpolation.component.html',
+  styleUrls: ['./interpolation.component.css']
 })
 
 export class InterpolationComponent {
-public name="Arun";
-public greeting="";
-onClick(){
-  
-  this.greeting ='Welcome to jumanji';
-}
+
 }
